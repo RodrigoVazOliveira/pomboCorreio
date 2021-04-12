1719b4b89278a0c5fe8ab10293645348b8821a7c
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-04-12 08:55:17 -0300

    Criar projeto spring com configuração do banco de dados MySQL

---
+++ src/test/java/br/com/zup/pombocorreio/PomboCorreioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.pombocorreio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PomboCorreioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

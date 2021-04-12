e9f3b5ba3dd7e79979f68bdf0f2dfaa5210dc43c
Rodrigo Vaz <rodrigovazdeoliveira@gmail.com>
2021-04-12 17:52:38 -0300

    criar endpoint

--- src/main/java/br/com/zup/pombocorreio/controllers/ContatoController.java
+++ src/main/java/br/com/zup/pombocorreio/controllers/ContatoController.java
@@ -1,9 +1,15 @@
 package br.com.zup.pombocorreio.controllers;
 
+import br.com.zup.pombocorreio.dtos.contato.CadastrarContatoDTO;
+import br.com.zup.pombocorreio.models.Conta;
+import br.com.zup.pombocorreio.models.Contato;
 import br.com.zup.pombocorreio.services.ContatoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("contatos/")
@@ -14,5 +20,13 @@ public class ContatoController {
     @Autowired
     public ContatoController(ContatoService contatoService) {
         this.contatoService = contatoService;
+    }
+
+    public SaidaCadastroContato gravarNovoContato(@RequestBody @Valid CadastrarContatoDTO cadastrarContatoDTO) {
+        Conta conta = new Conta();
+        conta.setId(cadastrarContatoDTO.getIdConta());
+        Contato contato = contatoService.gravarNovoContato(
+                cadastrarContatoDTO.converterDtoParaModelo(), conta
+        );
     }
 }

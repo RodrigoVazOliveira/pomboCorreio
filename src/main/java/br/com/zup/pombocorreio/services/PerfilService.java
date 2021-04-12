81ebe737949866f58d833ac707ecdeb7a8f2018a
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-04-12 10:20:18 -0300

    Criar método de buscar perfil por número de telefone

--- src/main/java/br/com/zup/pombocorreio/services/PerfilService.java
+++ src/main/java/br/com/zup/pombocorreio/services/PerfilService.java
@@ -1,7 +1,10 @@
 package br.com.zup.pombocorreio.services;
 
+import br.com.zup.pombocorreio.models.Perfil;
 import br.com.zup.pombocorreio.repositories.PerfilRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class PerfilService {
@@ -10,5 +13,15 @@ public class PerfilService {
 
     public PerfilService(PerfilRepository perfilRepository) {
         this.perfilRepository = perfilRepository;
+    }
+
+    public Perfil procurarPerfilPorNumeroTelefone(String numeroTelefone) {
+        Optional<Perfil> optionalPerfil = perfilRepository.findByNumeroTelefone(numeroTelefone);
+
+        if (optionalPerfil.isEmpty()) {
+            throw new RuntimeException("Um perfil já existe com o núemro de telefone " + numeroTelefone);
+        }
+
+        return optionalPerfil.get();
     }
 }

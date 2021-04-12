c98a10cd575d5273d13f1192011aa18d6ddcafcb
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-04-12 09:27:25 -0300

    Criar modelo de mensagem

---
+++ src/main/java/br/com/zup/pombocorreio/models/Mensagem.java
@@ -0,0 +1,61 @@
+package br.com.zup.pombocorreio.models;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Table(name = "mensagens")
+public class Mensagem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String mensagem;
+    private LocalDateTime dataHora;
+
+    public Mensagem() {
+    }
+
+    public Mensagem(String mensagem, LocalDateTime dataHora) {
+        this.mensagem = mensagem;
+        this.dataHora = dataHora;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public LocalDateTime getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(LocalDateTime dataHora) {
+        this.dataHora = dataHora;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Mensagem mensagem = (Mensagem) o;
+        return Objects.equals(id, mensagem.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}

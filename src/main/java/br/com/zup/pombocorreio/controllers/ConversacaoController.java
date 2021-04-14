7944ac0c1e17f8c531cb5739923f41416ea4e1bc
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-04-14 02:44:20 -0300

    Criar endpoint para iniciar conversação

--- src/main/java/br/com/zup/pombocorreio/controllers/ConversacaoController.java
+++ src/main/java/br/com/zup/pombocorreio/controllers/ConversacaoController.java
@@ -1,9 +1,14 @@
 package br.com.zup.pombocorreio.controllers;
 
+import br.com.zup.pombocorreio.dtos.conversacao.IniciarConversacaoDTO;
+import br.com.zup.pombocorreio.dtos.conversacao.SaidaNovaConversacaoDTO;
+import br.com.zup.pombocorreio.models.Conversacao;
 import br.com.zup.pombocorreio.services.ConversacaoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("conversacoes/")
@@ -16,5 +21,14 @@ public class ConversacaoController {
         this.conversacaoService = conversacaoService;
     }
 
-
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public SaidaNovaConversacaoDTO iniciarNovaConversacao(@RequestBody
+                                                          @Valid
+                                                          IniciarConversacaoDTO iniciarConversacaoDTO) {
+        Conversacao conversacao = conversacaoService.gravarNovaConversacao(
+                iniciarConversacaoDTO.converterDtoParaModelo()
+        );
+        return SaidaNovaConversacaoDTO.converterModeloParaDto(conversacao);
+    }
 }

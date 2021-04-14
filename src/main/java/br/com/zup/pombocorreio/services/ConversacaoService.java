c372007b57300b65e854dafbb95d5be414f38c77
Rodrigo Vaz <rodrigovazdeoliveira@gmail.com>
2021-04-14 09:47:57 -0300

    Criar método para obter todas conversas por id da conta

--- src/main/java/br/com/zup/pombocorreio/services/ConversacaoService.java
+++ src/main/java/br/com/zup/pombocorreio/services/ConversacaoService.java
@@ -7,7 +7,6 @@ import br.com.zup.pombocorreio.repositories.ConversacaoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -43,5 +42,10 @@ public class ConversacaoService {
         }
 
         return optionalConversacao.get();
+    }
+
+    public Iterable<Conversacao> obterTodasConversasPorIdDaConta(Long id) {
+        Conta contaAntiga = contaService.procurarContaPorId(id);
+        return conversacaoRepository.findByConta(contaAntiga);
     }
 }

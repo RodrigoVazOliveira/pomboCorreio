3fc7b41f9f1ac7e4e90b43cb45589ab14c6f8090
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-04-14 03:03:18 -0300

    Criar método personalizado para verificar se conversa já existe

--- src/main/java/br/com/zup/pombocorreio/repositories/ConversacaoRepository.java
+++ src/main/java/br/com/zup/pombocorreio/repositories/ConversacaoRepository.java
@@ -1,7 +1,10 @@
 package br.com.zup.pombocorreio.repositories;
 
+import br.com.zup.pombocorreio.models.Conta;
+import br.com.zup.pombocorreio.models.Contato;
 import br.com.zup.pombocorreio.models.Conversacao;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ConversacaoRepository extends CrudRepository<Conversacao, Long> {
+    Conversacao existsByContaAndContato(Conta conta, Contato contato);
 }

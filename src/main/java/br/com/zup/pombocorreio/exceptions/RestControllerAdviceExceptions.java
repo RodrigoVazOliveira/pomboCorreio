3f3c4f9f0d5e0dec2178c7fa3d1eca5129e82342
Rodrigo Vaz <rodrigovazdeoliveira@gmail.com>
2021-04-14 11:13:46 -0300

    criar classe para tratamento de exceções no rest controller

---
+++ src/main/java/br/com/zup/pombocorreio/exceptions/RestControllerAdviceExceptions.java
@@ -0,0 +1,10 @@
+package br.com.zup.pombocorreio.exceptions;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class RestControllerAdviceExceptions  extends ResponseEntityExceptionHandler {
+
+    
+}
